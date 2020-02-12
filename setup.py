5f025a468e76715ca8f6018989e3305ed50d7c1a
Joel C. Zinn <zinn.44@osu.edu>
2020-02-12 15:25:33 +1100

    making a package...

---
+++ setup.py
@@ -0,0 +1,3 @@
+from setuptools import setup
+__version__ = '0.1'
+setup(name='my_routines', author='Joel C. Zinn', install_requires=["pyfits", "numpy", "pandas", "scipy", "matplotlib", "pylab"], packages=['./'])
