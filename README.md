6ab80d11bd2db48472058a1df59650b79105c68b
Joel C. Zinn <zinn.44@osu.edu>
2024-04-02 14:53:16 -0700

    added const.py and updated mcmc_class.py

--- README.md
+++ README.md
@@ -5,5 +5,8 @@ To install the code necessary to run these scripts, do:\
 cd my_routines/\
 python setup.py install
 
+v0.3
+removed pyfits requirement in favor of astropy
+
 v0.2
 added mcmc_class.py
--- setup.py
+++ setup.py
@@ -1,3 +1,3 @@
 from setuptools import setup
-__version__ = '0.2'
-setup(name='my_routines', author='Joel C. Zinn', install_requires=["pyfits", "numpy", "pandas", "scipy", "matplotlib", "emcee", 'corner', 'pudb'], packages=['./'])
+__version__ = '0.3'
+setup(name='my_routines', author='Joel C. Zinn', install_requires=["astropy", "numpy", "pandas", "scipy", "matplotlib", "emcee", 'corner', 'pudb', 'matplotlib', 'pylab', 'glob', 'mesa_reader'], packages=['./'])
