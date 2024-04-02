2f8cbe275da59241db251c19129e18f95042c95b
Joel C. Zinn <zinn.44@osu.edu>
2024-04-02 14:53:16 -0700

    added const.py and updated mcmc_class.py

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
 from setuptools import setup
 __version__ = '0.3'
-setup(name='my_routines', author='Joel C. Zinn', install_requires=["astropy", "numpy", "pandas", "scipy", "matplotlib", "emcee", 'corner', 'pudb', 'matplotlib', 'glob', 'mesa_reader'], packages=['./'])
+setup(name='my_routines', author='Joel C. Zinn', install_requires=["astropy", "numpy", "pandas", "scipy", "matplotlib", "emcee", 'corner', 'pudb', 'matplotlib', 'glob'], packages=['./'])
+
