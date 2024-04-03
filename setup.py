c0d24e8cb0e8db56a3439912c8be7ad662bb1c42
Joel C. Zinn <zinn.44@osu.edu>
2024-04-02 19:14:53 -0700

    moved things around to work with pip install .

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
 from setuptools import setup
 __version__ = '0.3'
-setup(name='my_routines', author='Joel C. Zinn', install_requires=["astropy", "numpy", "pandas", "scipy", "matplotlib", "emcee", 'corner', 'pudb', 'matplotlib', 'glob'], packages=['./'])
+setup(name='my_routines', author='Joel C. Zinn', install_requires=["astropy", "numpy", "pandas", "scipy", "matplotlib", "emcee", 'corner', 'pudb'], packages=['my_routines'])
 
